9dcd796e712810d53b18930ccc1c4542b68c09d1
Eric Moore <EricGittaMoore@duck.com>
2025-05-07 21:14:37 +0800

    chore: 更新 .gitignore 文件以包含 .env，更新 example.env 文件以添加 DEFAULT_TEXT_SEARCH_CONFIG 环境变量，并修改 PostgreSQL 初始化脚本以使用新的中文文本搜索配置

--- postgresql/init-extensions.sql
+++ postgresql/init-extensions.sql
@@ -4,6 +4,6 @@ CREATE EXTENSION IF NOT EXISTS vectors;
 -- 开启中文分词器
 CREATE EXTENSION IF NOT EXISTS zhparser;
 -- make test configuration using parser
-CREATE TEXT SEARCH CONFIGURATION zhcfg (PARSER = zhparser);
+CREATE TEXT SEARCH CONFIGURATION chinese (PARSER = zhparser);
 -- add token mapping
-ALTER TEXT SEARCH CONFIGURATION zhcfg ADD MAPPING FOR n,v,a,i,e,l WITH simple;
+ALTER TEXT SEARCH CONFIGURATION chinese ADD MAPPING FOR n,v,a,i,e,l WITH simple;
